a6437a790d1737ae8fceca14ef32d48bd01f0cea
Edfood <kfed542@gmail.com>
2020-09-17 08:24:20 +0900

    バックエンドを追加

---
+++ backend/router.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/CountdownToDo/backend/handler"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func newRouter() *echo.Echo {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Static("/assets", "public/assets")
+
+	// e.File("/", "public/index.html")
+	// e.File("/signup", "public/signup.html")
+	e.POST("/signup", handler.Signup)
+	// e.File("/login", "public/signin.html")
+	e.POST("/signin", handler.Signin)
+	// e.File("/tasks", "public/todos.html")
+
+	api := e.Group("/api")
+	api.Use(middleware.JWTWithConfig(handler.Config))
+	api.POST("/api/tasks", handler.AddTask)
+	api.GET("/api/tasks", handler.GetTasks)
+	api.DELETE("/tasks/:id", handler.DeleteTask)
+
+	return e
+}
