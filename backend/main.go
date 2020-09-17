b80c965f436c6ca137c44d4f24c12a6e3f842b97
Edfood <kfed542@gmail.com>
2020-09-17 15:09:53 +0900

    ポート番号を環境変数から指定

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,12 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
 
 	router := newRouter()
-	router.Logger.Fatal(router.Start(":8080"))
+	router.Logger.Fatal(router.Start(":" + os.Getenv("PORT")))
 
 }
